5e8c689a05f6de48c9c18d72361b1a9e75d51eea
dimosoff <dimmiostr@mail.ru>
2023-03-20 17:00:20 +0300

    prod-page 70%

--- assets/templates/new/js/main.js
+++ assets/templates/new/js/main.js
@@ -4,11 +4,11 @@ const burgerMenu = document.querySelector(".burger-menu");
 
 let menuBtn = () => {
   if (iconMenu.classList.contains("_active")) {
-    document.body.classList.remove("_lock");
+    // document.body.classList.remove("_lock");
     iconMenu.classList.remove("_active");
     menuBody.classList.remove("_active");
   } else {
-    document.body.classList.add("_lock");
+    // document.body.classList.add("_lock");
     iconMenu.classList.add("_active");
     menuBody.classList.add("_active");
   }
