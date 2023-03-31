3b0f5b067df59f8bd4f7a376dca4f5e82e4ad2d3
dimosoff <dimmiostr@mail.ru>
2023-03-31 13:15:08 +0300

    homepage fixes

--- assets/templates/new/js/popular-goods.js
+++ assets/templates/new/js/popular-goods.js
@@ -1,44 +1,49 @@
 new Swiper('.popular-goods__slider',{
     slideToClickedSlide: true,
-    slidesPerView: 6,
+    slidesPerView: 4,
     // spaceBetween: 26,
     slidesPerGroup: 1,
     touchRatio: 1,
     grabCursor: true,
     // autoHeight: true,
     // initialSlide: 1,
-    speed: 800,
+    speed: 1500,
     dynamicBullets: true,
     pagination:{
         el: '.bullets-custom-popular__goods',
         clickable: true,
     },
     autoplay: {
-        delay: 4000,
+        delay: 5000,
     },
     breakpoints: {
-        // when window width is >= 320px
-        320: {
+        // when window width is >= 0px
+        0: {
             slidesPerView: 1.5,
+            slidesPerGroup: 1,
             centeredSlides: true,
             initialSlide: 1,
         },
-        500: {
-            slidesPerView: 3.5,
+        575: {
+            slidesPerView: 2.5,
+            slidesPerGroup: 2,
             initialSlide: 1,
             slideActiveClass: 'active-slide',
             centeredSlides: true,
             // spaceBetween: 20
         },
-        // when window width is >= 480px
-        700: {
-            slidesPerView: 5,
+        767: {
+            slidesPerView: 3.5,
+            slidesPerGroup: 3,
+            initialSlide: 1,
+            slideActiveClass: 'active-slide',
+            centeredSlides: true,
             // spaceBetween: 20
         },
-        // when window width is >= 640px
-        1250: {
-            slidesPerView: 6,
-            // spaceBetween: 26
+        991: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+            // spaceBetween: 20
         }
     }
 })
